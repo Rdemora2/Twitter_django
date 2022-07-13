12eacd3fff341d4dba818820ef34a63a8ce70fca
Roberto Moraes <robertomoraeszar@gmail.com>
2022-07-13 14:23:18 -0300

    add requirements.txt

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,4 +1,4 @@
-"""oinkoink URL Configuration
+"""twitter URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
